c4d41acc4a70ca81b386573f52d8b21e26c8d56d
Ilia Ilinykh <IIlyinykh@alfabank.ru>
2020-06-04 22:01:44 +0300

    Initial Prep Commit

---
+++ src/test/java/com/example/validation/ValidationApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ValidationApplicationTests {
+
+    @Test
+    @DisplayName("Context loads fine.")
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
